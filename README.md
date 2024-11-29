1de9fea8baee894bf65fbf9a15ccb0bef13ba163
Malte Gejr <maltegejr.korup@gmail.com>
2024-11-29 07:45:17 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,20 @@
+# cmdlog
+[![Go Reference](https://pkg.go.dev/badge/github.com/TheGejr/cmdlog.svg)](https://pkg.go.dev/github.com/TheGejr/cmdlog) ![GitHub Workflow Status (with event)](https://img.shields.io/github/actions/workflow/status/TheGejr/cmdlog/release.yml) ![GitHub](https://img.shields.io/github/license/TheGejr/cmdlog) ![GitHub release (with filter)](https://img.shields.io/github/v/release/TheGejr/cmdlog)
+
+
+
+
+
+## Installation and Usage
+Install this program:
+```
+$ go install github.com/TheGejr/cmdlog@latest
+```
+
+```
+USAGE: cmdlog [OPTION...] CMD [CMD OPTION...]
+
+$ cmdlog --help
+```
+
+## Example usage
---
+++ cmdlog.go
@@ -0,0 +1,129 @@
+package main
+
+// Issue 01:
+// Color is not preserved...
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"regexp"
+)
+
+var VERSION = "1.0.0"
+
+func main() {
+	// Parse options (if any)
+	flag.Parse()
+
+	// Remaining arguments are the command and its options
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Usage: cmdlog [OPTION...] CMD [CMD OPTION...]")
+    fmt.Println("cmdlog prints to stdout and stderr and logs to a logfile. Easy to use,")
+    fmt.Println("easy to document.")
+    fmt.Printf("\ncmdlog v%s was created by Malte Gejr <malte@gejr.dk>\n", VERSION)
+		os.Exit(1)
+	}
+
+	// The first argument is the command name
+	cmdName := args[0]
+	cmdArgs := args[1:]
+
+	// Generate the log filename based on the command and its options
+	logFileName := generateLogFileName(cmdName, cmdArgs)
+
+	// Create or open the log file (after ensuring unique file name)
+	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Error opening log file: %v", err)
+	}
+	defer logFile.Close()
+
+	// Create a logger that writes to both stdout and the log file
+	logger := log.New(io.MultiWriter(os.Stdout, logFile), "", log.LstdFlags)
+
+	// Print the CMD and CMD OPTIONS to the log file and stdout
+ 	cmdLine := fmt.Sprintf("%s %s", cmdName, strings.Join(cmdArgs, " "))
+ 	fmt.Fprintf(logFile, "%s\n\n", cmdLine)
+
+	// Set up the command to execute
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmdStdout, err := cmd.StdoutPipe()
+	if err != nil {
+		logger.Fatalf("Error setting up stdout pipe: %v", err)
+	}
+	cmdStderr, err := cmd.StderrPipe()
+	if err != nil {
+		logger.Fatalf("Error setting up stderr pipe: %v", err)
+	}
+
+	// Start the command
+	err = cmd.Start()
+	if err != nil {
+		logger.Fatalf("Error starting command: %v", err)
+	}
+
+	// Create goroutines to copy the command's stdout and stderr to the logger
+	go func() {
+		_, _ = io.Copy(logger.Writer(), cmdStdout)
+	}()
+	go func() {
+		_, _ = io.Copy(logger.Writer(), cmdStderr)
+	}()
+
+	// Wait for the command to finish
+	err = cmd.Wait()
+	if err != nil {
+		log.Printf("Command finished with error: %v", err)
+	}
+}
+
+// generateLogFileName creates a log file name based on the command and its arguments.
+// It replaces spaces with underscores and sanitizes the filename to avoid invalid characters.
+// It checks if a file already exists and appends a number to the filename if necessary.
+func generateLogFileName(cmdName string, cmdArgs []string) string {
+	// Combine the command name and its arguments into a single string
+	cmdString := cmdName + " " + strings.Join(cmdArgs, " ")
+
+	// Sanitize the filename by replacing invalid characters
+	cmdString = sanitizeFilename(cmdString)
+
+	// Append ".log" to create the final log file name
+	baseFileName := "cmdlog_" + cmdString + ".log"
+	logFileName := baseFileName
+
+	// Check if the log file already exists, and increment the file name if it does
+	counter := 1
+	for fileExists(logFileName) {
+		logFileName = fmt.Sprintf("%s.%d.log", baseFileName[:len(baseFileName)-4], counter)
+		counter++
+	}
+
+	return logFileName
+}
+
+// fileExists checks if a file exists at the given path.
+func fileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return !os.IsNotExist(err)
+}
+
+// sanitizeFilename replaces characters that are invalid in filenames with underscores.
+func sanitizeFilename(s string) string {
+	// Replace invalid characters with underscores or dashes
+	re := regexp.MustCompile(`[\\\/:*?"<>|&^%$#@!~;,+\s]`)
+	s = re.ReplaceAllString(s, "_")
+
+	// Ensure that the filename doesn't start with an underscore or dash (if necessary)
+	if len(s) > 0 && (s[0] == '_' || s[0] == '-') {
+		s = "cmdlog_" + s[1:]
+	}
+
+	return s
+}
+
